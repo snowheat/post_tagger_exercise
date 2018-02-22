f9a37c5adc82dee055dd69dc071fd84363c16ef4
snowheat <musiklib@gmail.com>
2018-02-22 16:17:20 +0700

    Initial commit

---
+++ post_tagger.py
@@ -0,0 +1,20 @@
+
+class PostTagger:
+
+    def __init__(self):
+        pass
+
+    def __get_corpus(self):
+        pass
+
+    def __get_train_data(self):
+        pass
+
+    def __get_test_data(self):
+        pass
+
+    def __train(self):
+        pass
+
+    def __test(self):
+        pass
