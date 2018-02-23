39c1e72bcffefa9fd28e736c364834b80b47cfb6
sigitbn <sigitkariagil@gmail.com>
2018-02-23 19:34:04 +0700

    hmm

--- post_tagger.py
+++ post_tagger.py
@@ -1,12 +1,12 @@
+from experiments.hmm import HMM
+
 
 class PostTagger:
-
     def __init__(self):
         self.__dataset_basic = []
         self.__dataset_hmm = []
         self.__dataset_non_hmm = []
         self.__corpus = self.__get_corpus();
-
 
     def __get_corpus(self):
         with open('id_pud-ud-test.conllu', 'r', encoding='utf8') as fp:
@@ -24,14 +24,17 @@ class PostTagger:
             row_data = []
 
             for term in row[2:]:
+                # print(term)
                 term_part = term.split()
                 row_data.append([term_part[1], term_part[3]])
 
 
             self.__dataset_basic.append(row_data)
 
-        print(self.__dataset_basic)
-
+        # print(self.__dataset_basic[901:])
+        self.__HMM = HMM()
+        self.__HMM.train(self.__dataset_basic[:900])
+        self.__HMM.test(self.__dataset_basic[901:])
 
     def __get_train_data(self):
         pass
