83cea9158e924012f0bb77a0a7712efaa764aece
snowheat <musiklib@gmail.com>
2018-02-23 15:26:38 +0700

    2

--- post_tagger.py
+++ post_tagger.py
@@ -1,11 +1,14 @@
 
 class PostTagger:
 
-    def __init__(self):
-        self.__dataset_basic = []
+    def __init__(self, training_parameters={}, testing_parameters={}):
+
+        self.__training_parameters = training_parameters
+        self.__testing_parameters = testing_parameters
         self.__dataset_hmm = []
-        self.__dataset_non_hmm = []
+
         self.__corpus = self.__get_corpus();
+        self.__dataset_non_hmm = self.__get_dataset_non_hmm(self.__corpus);
 
 
     def __get_corpus(self):
@@ -15,7 +18,7 @@ class PostTagger:
 
         fp.close()
 
-        self.__dataset_basic = []
+        corpus = []
 
         for block in all_text:
 
@@ -27,10 +30,9 @@ class PostTagger:
                 term_part = term.split()
                 row_data.append([term_part[1], term_part[3]])
 
+            corpus.append(row_data)
 
-            self.__dataset_basic.append(row_data)
-
-        print(self.__dataset_basic)
+        return corpus
 
 
     def __get_train_data(self):
@@ -44,3 +46,25 @@ class PostTagger:
 
     def __test(self):
         pass
+
+    def __get_dataset_non_hmm(self, corpus):
+        feature_array = []
+
+        for text in corpus:
+
+            for index, word_data in enumerate(text):
+
+                if index == 0:
+                    pass
+
+                elif index == len(text)-1:
+                    pass
+
+                else:
+                    pass
+
+                print(index+word_data[0])
+
+
+
+        print(corpus)
