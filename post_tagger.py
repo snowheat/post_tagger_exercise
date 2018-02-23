430264f00e2cb38f55527b04547b49aeaa1d91a0
snowheat <musiklib@gmail.com>
2018-02-23 14:14:44 +0700

    dataset basic

--- post_tagger.py
+++ post_tagger.py
@@ -3,6 +3,9 @@ class PostTagger:
 
     def __init__(self):
         self.__corpus = self.__get_corpus();
+        self.__dataset_basic = []
+        self.__dataset_hmm = []
+        self.__dataset_non_hmm = []
 
     def __get_corpus(self):
         with open('id_pud-ud-test.conllu', 'r', encoding='utf8') as fp:
@@ -11,7 +14,22 @@ class PostTagger:
 
         fp.close()
 
-        print(len(all_text))
+        self.__dataset_basic = []
+
+        for block in all_text:
+
+            row = block.split('\n')
+
+            row_data = []
+
+            for term in row[2:]:
+                term_part = term.split()
+                row_data.append([term_part[1], term_part[3]])
+
+
+            self.__dataset_basic.append(row_data)
+
+        print(self.__dataset_basic)
 
 
     def __get_train_data(self):
